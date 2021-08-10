66318013767507568c1a36f96dd5e3270c8cb450
mm02417 <mm02417@surrey.ac.uk>
2021-08-10 15:28:46 +0100

    Adds basic responsive design with Bootstrap

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
+import 'bootstrap/dist/css/bootstrap.css';
 import { BrowserRouter as Router } from "react-router-dom";
 // import reportWebVitals from './reportWebVitals';
 
