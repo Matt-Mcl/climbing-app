e9aa82030949f4932ca62b663ed4e04e7de11a32
mm02417 <mm02417@surrey.ac.uk>
2021-08-10 18:49:30 +0100

    Changes default day to current day.
    Adds slight styling changes

--- frontend/src/Graph.js
+++ frontend/src/Graph.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 import Form from 'react-bootstrap/Form'
 import Col from 'react-bootstrap/Col'
-// import Container from 'react-bootstrap/Container'
 import Image from 'react-bootstrap/Image'
+import Row from 'react-bootstrap/Row'
 
 class Graph extends Component {
   constructor() {
     super();
-    this.state = {day: 'monday', show: false};
+    this.state = {day: 'today', show: true};
     this.handleDayChange = this.handleDayChange.bind(this)
     this.handleShowChange = this.handleShowChange.bind(this)
   }
@@ -23,11 +23,12 @@ class Graph extends Component {
   render() {
     return (
       <>
-        <Col md={4}>
-          <Form>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form>
+          <Row style={{marginTop: "16px"}}>
+            <Col xs="auto">
               <Form.Label>Day:</Form.Label>
               <Form.Select value={this.state.day} onChange={this.handleDayChange}>
+                <option></option>
                 <option value="monday">Monday</option>
                 <option value="tuesday">Tuesday</option>
                 <option value="wednesday">Wednesday</option>
@@ -36,14 +37,13 @@ class Graph extends Component {
                 <option value="saturday">Saturday</option>
                 <option value="sunday">Sunday</option>
               </Form.Select>
-            </Form.Group>
-
-            <Form.Group className="mb-3" controlId="formBasicEmail">
+            </Col>
+            <Col xs="auto">
               <Form.Label>Show:</Form.Label>
               <Form.Check type="checkbox" checked={this.state.show} onChange={this.handleShowChange} />
-            </Form.Group>
-          </Form>
-        </Col>
+            </Col>
+          </Row>
+        </Form>
         <Image src={`${process.env.REACT_APP_API_SERVER}/getgraph?day=${this.state.day}&show=${this.state.show}`} alt="graph of climbing data" fluid />
       </>
     );
