0e64f309de145538b0376ec41f73ca00da9a2cdc
Matt-Mcl <mattmcl2000@gmail.com>
2021-09-29 18:29:27 +0100

    Changes climbing data URL

--- backend/functions/climbing.js
+++ backend/functions/climbing.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch');
 
 module.exports = { 
   async getClimbingCount() {
-    const response = await fetch('https://portal.rockgympro.com/portal/public/2660c1de4a602e808732f0bcd3fea712/occupancy?&iframeid=occupancyCounter&fId=');
+    const response = await fetch('https://portal.rockgympro.com/portal/public/2660c1de4a602e808732f0bcd3fea712/occupancy');
     const text = await response.text();
     // Retrieves the count and capacity with a regex and removes all non-numbers
     const count = parseInt(text.match(/('count' : ).+/)[0].replace(/[^0-9]/g, ''));
