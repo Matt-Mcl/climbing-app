e9ca037725a52c830b1d7520fec9cdefe4840812
Matt-Mcl <mattmcl2000@gmail.com>
2021-12-08 16:53:53 +0000

    Removes old "No data available" comment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -62,7 +62,6 @@ function App() {
         <Container style={{marginTop: "32px"}}>
           <Row>
             <Col>
-              <p>Note: No data available for 23/10/2021 - 24/10/2021</p>
               <p>{new Date().getFullYear()} Copyright: <a href="https://climbing-app.co.uk">climbing-app.co.uk</a></p>
             </Col>
             <Col xs="auto">
